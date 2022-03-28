c469d266740fee94788c1c2385d352075e58ddc0
Ingrid Aquino <ingridaquinosoares@live.com>
2022-03-28 16:40:25 -0300

    :construction: Menu responsivo foi criadom, ainda falta alguns reajustes.

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let menuIcon = document.getElementById('buguer');
+
+
+// let navBar = document.getElementById('listBar');
+// let list = navBar.getElementsByClassName('li');
+
+// menuIcon.addEventListener("Click", () => {
+//     navBar.classList.toggle("show");
+// });
+
+// list.forEach((list) => {
+//     list.addEventListener("click", () => {
+//         navBar.classList.remove("show")
+//     });
+// });
+
+
